4c87899bfe78bfd1f47b4e5e4ddc2b1976c3b187
Kisoon Kwon <moxie2ks@gmail.com>
2023-02-16 13:18:26 +0900

    Make thumbnails in every seconds.

--- example/ThumbnailGenerator/ThumbnailGenerator/ViewController.m
+++ example/ThumbnailGenerator/ThumbnailGenerator/ViewController.m
@@ -22,64 +22,68 @@
 
     
 -(void)extractImagesFromMovie {
-    NSURL* movURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"test" ofType:@"mp4"]];
+    NSURL* url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"test" ofType:@"mp4"]];
 
-    NSMutableDictionary* myDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES] ,
+    NSMutableDictionary* dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES] ,
                                      AVURLAssetPreferPreciseDurationAndTimingKey ,
                                     [NSNumber numberWithInt:0],
                                     AVURLAssetReferenceRestrictionsKey, nil];
 
-    AVURLAsset* movie = [[AVURLAsset alloc] initWithURL:movURL options:myDict];
+    AVURLAsset* avUrlAsset = [[AVURLAsset alloc] initWithURL:url options:dict];
 
     // set the generator
-    AVAssetImageGenerator* generator = [AVAssetImageGenerator assetImageGeneratorWithAsset:movie];
+    AVAssetImageGenerator* generator = [AVAssetImageGenerator assetImageGeneratorWithAsset:avUrlAsset];
     generator.requestedTimeToleranceBefore = kCMTimeZero;
     generator.requestedTimeToleranceAfter = kCMTimeZero;
     generator.maximumSize = CGSizeMake(192, 144);
+
     // look for the video track
-    AVAssetTrack* videoTrack;
+    AVAssetTrack* videoTrack = nil;
     bool foundTrack = NO;
 
-    for (AVAssetTrack* track in movie.tracks) {
+    for (AVAssetTrack* track in avUrlAsset.tracks) {
         if ([track.mediaType isEqualToString:@"vide"]) {
             if (foundTrack) {
                 NSLog (@"Error - - - more than one video tracks");
                 return;
-            }
-            else {
+            } else {
                 videoTrack = track;
                 foundTrack = YES;
             }
         }
     }
     
-    if (foundTrack == NO) {
+    if (videoTrack == nil) {
         NSLog (@"Error - - No Video Tracks at all");
         return;
     }
 
     // set the number of frames in the movie
     int frameRate = videoTrack.nominalFrameRate;
-    float value = movie.duration.value;
-    float timeScale = movie.duration.timescale;
+    float value = avUrlAsset.duration.value;
+    float timeScale = avUrlAsset.duration.timescale;
     float totalSeconds = value / timeScale;
     int totalFrames = totalSeconds * frameRate;
-    int timeValuePerFrame = movie.duration.timescale / frameRate;
+    int timeValuePerFrame = avUrlAsset.duration.timescale / frameRate;
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSMutableArray *times=[[NSMutableArray alloc] init];
 
     // get each frame
-    for (int k=0; k< totalFrames; k++) {
-        int timeValue = timeValuePerFrame * k;
-        CMTime frameTime;
-        frameTime.value = timeValue;
-        frameTime.timescale = movie.duration.timescale;
-        frameTime.flags = movie.duration.flags;
-        frameTime.epoch = movie.duration.epoch;
-        NSLog (@"Frame %d Time Stamp %lld", k, frameTime.value*1000/frameTime.timescale);
-        [times addObject:[NSValue valueWithCMTime:frameTime]];
+    for (int k=1; k<=totalFrames; k++) {
+        unsigned long timeValue = timeValuePerFrame * k;
+        unsigned int timeStampMsec = timeValue*1000 / timeScale;
+
+        // Set times every seconds : 1st frame + frame at 1000*n msec.
+        if (k == 1 || timeStampMsec % 1000 == 0) {
+            CMTime frameTime;
+            frameTime.value = timeValue;
+            frameTime.timescale = timeScale;
+            frameTime.flags = avUrlAsset.duration.flags;
+            frameTime.epoch = avUrlAsset.duration.epoch;
+            NSLog (@"Set time at Frame %d Time Stamp %lld", k, frameTime.value*1000/frameTime.timescale);
+            [times addObject:[NSValue valueWithCMTime:frameTime]];
+        }
     }
-    
     
     [generator generateCGImagesAsynchronouslyForTimes:times
                                     completionHandler:^(CMTime requestedTime,
@@ -89,12 +93,11 @@
                                                         NSError *error) {
         NSString *requestedTimeString = (__bridge NSString *) CMTimeCopyDescription (NULL, requestedTime);
         NSString *actualTimeString = (__bridge NSString *) CMTimeCopyDescription(NULL, actualTime);
-        NSLog(@"Requested: %@; actual %@", requestedTimeString, actualTimeString);
 
         if (result == AVAssetImageGeneratorSucceeded) {
             if (actualTime.value != requestedTime.value) {
                 UIImage *img = [UIImage imageWithCGImage:image];
-                NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:[NSString stringWithFormat:@"test_%lld.png",actualTime.value*1000/actualTime.timescale]];
+                NSString *filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:[NSString stringWithFormat:@"%lld_sec.png",requestedTime.value/requestedTime.timescale]];
     
                 NSData *data = [NSData dataWithData:UIImagePNGRepresentation(img)];
                 NSError *writeError = nil;
@@ -102,17 +105,15 @@
                 [data writeToFile:filePath options:NSDataWritingAtomic error:&writeError];
     
                 if (writeError) {
-                  NSLog(@"Error writing file: %@", writeError);
+                    NSLog(@"Error writing file: %@", writeError);
+                } else {
+                    NSLog(@"Generated '%lld_sec.png'", requestedTime.value/requestedTime.timescale);
                 }
             }
-        }
-
-        if (result == AVAssetImageGeneratorFailed) {
-          NSLog(@"Failed with error: %@", [error localizedDescription]);
-        }
-
-        if (result == AVAssetImageGeneratorCancelled) {
-          NSLog(@"Canceled");
+        } else if (result == AVAssetImageGeneratorFailed) {
+            NSLog(@"Failed with error: %@", [error localizedDescription]);
+        } else if (result == AVAssetImageGeneratorCancelled) {
+            NSLog(@"Canceled");
         }
     }];
 }
