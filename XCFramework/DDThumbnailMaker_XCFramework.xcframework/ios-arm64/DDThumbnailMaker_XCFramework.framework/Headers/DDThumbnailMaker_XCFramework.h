ad5276ba66838838a3bbd1bd37733c603413c027
Kisoon Kwon <moxie2ks@gmail.com>
2023-02-27 17:10:24 +0900

    Added XCFramework build project.

---
+++ XCFramework/DDThumbnailMaker_XCFramework.xcframework/ios-arm64/DDThumbnailMaker_XCFramework.framework/Headers/DDThumbnailMaker_XCFramework.h
@@ -0,0 +1,18 @@
+//
+//  DDThumbnailMaker_XCFramework.h
+//  DDThumbnailMaker_XCFramework
+//
+//  Created by Daniel on 2023/02/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DDThumbnailMaker_XCFramework.
+FOUNDATION_EXPORT double DDThumbnailMaker_XCFrameworkVersionNumber;
+
+//! Project version string for DDThumbnailMaker_XCFramework.
+FOUNDATION_EXPORT const unsigned char DDThumbnailMaker_XCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDThumbnailMaker_XCFramework/PublicHeader.h>
+
+
---
+++ XCFramework/build_xcframework.sh
@@ -0,0 +1,9 @@
+xcodebuild archive -scheme DDThumbnailMaker -archivePath './build/DDThumbnailMaker.xcarchive' -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme DDThumbnailMaker -archivePath './build/DDThumbnailMaker_sim.xcarchive' -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+xcodebuild -create-xcframework \
+    -framework "./build/DDThumbnailMaker.xcarchive/Products/Library/Frameworks/DDThumbnailMaker.framework" \
+    -framework "./build/DDThumbnailMaker_sim.xcarchive/Products/Library/Frameworks/DDThumbnailMaker.framework" \
+    -output "./DDThumbnailMaker.xcframework"
+
+rm -rf build
