bd894301ade487a062239a59b0c63e9c8caeace9
Kisoon Kwon <moxie2ks@gmail.com>
2023-03-19 22:45:48 +0900

    Optimized modules.  - Splited completion handlers with imageHandler & completion  - Support variables of video info, videoTracks, frameRate and duration after init  - Modified test code following with changed API.

--- DDThumbnailMaker/Classes/DDThumbnailMaker.swift
+++ DDThumbnailMaker/Classes/DDThumbnailMaker.swift
@@ -10,40 +10,73 @@ import UIKit
 import AVKit
 
 public class DDThumbnailMaker {
-    public var avAsset : AVAsset? = nil
-    public var intervalMsec: UInt? = 1000
-    public var intervalFrame: UInt? = 0
+    public var avAsset: AVAsset!
+    public var intervalMsec: UInt! = 1000
+    public var intervalFrame: UInt! = 0
     public var thumbnailImageSize: CGSize? = CGSize(width: 192, height: 144)
+    public var videoTracks: [AVAssetTrack]!
+    public var frameRate: Int!
+    public var duration: CMTime!
     
-    private var generator : AVAssetImageGenerator? = nil
+    private var generator: AVAssetImageGenerator? = nil
 
     public init(_ avAsset: AVAsset) {
         self.avAsset = avAsset
+        self.initAsset()
     }
     
     public init(_ url:URL) {
         self.avAsset = AVAsset(url: url)
+        self.initAsset()
     }
     
-    public func generate(_ completionHandler:@escaping (CMTime, CGImage?, CMTime, AVAssetImageGenerator.Result, NSError?) ->Void) {
+    private func initAsset() {
+        let semaphore = DispatchSemaphore(value: 0)
         Task {
-            await generateInternal(completionHandler)
+            await initAssetInternal()
+            semaphore.signal()
+        }
+        semaphore.wait()
+    }
+    
+    private func initAssetInternal() async {
+        self.videoTracks = try? await self.avAsset.loadTracks(withMediaType: .video)
+        self.frameRate = self.videoTracks.count > 0 ? try! await Int(self.videoTracks.first!.load(.nominalFrameRate)) : 0
+        self.duration = try? await self.avAsset.load(.duration)
+        
+        let size = try! await self.videoTracks.first!.load(.naturalSize).applying(self.videoTracks.first!.load(.preferredTransform))
+        self.thumbnailImageSize = CGSize(width: abs(size.width), height: abs(size.height))
+    }
+    
+    public func generate(imageHandler:@escaping (CMTime, CGImage?, CMTime, AVAssetImageGenerator.Result, NSError?) ->Void,
+                         completion:@escaping () -> Void) {
+        Task {
+            await generateInternal(imageHandler: imageHandler, completion: completion)
         }
     }
     
-    private func generateInternal(_ completionHandler:@escaping (CMTime, CGImage?, CMTime, AVAssetImageGenerator.Result, NSError?) ->Void) async {
+    private func generateInternal(imageHandler:@escaping (CMTime, CGImage?, CMTime, AVAssetImageGenerator.Result, NSError?) ->Void,
+                                  completion:@escaping () -> Void) async {
         // Get requested asset times
-        let times = await getRequestedAssetTimes(self.avAsset!)
+        let times = await getRequestedAssetTimes()
 
         // AVAssetImageGenerator work
-        let generator = getAVAssetImageGenerator(self.avAsset!)
+        var count = 0
+        let frameCount = times.count
+        let generator = getAVAssetImageGenerator()
         generator.generateCGImagesAsynchronously(forTimes: times) { (requestedTime, image, actualTime, result, error) in
-            completionHandler(requestedTime, image, actualTime, result, error as NSError?)
+            count += 1
+            defer {
+                if count == frameCount {
+                    DispatchQueue.main.async { completion() }
+                }
+            }
+            imageHandler(requestedTime, image, actualTime, result, error as NSError?)
         }
     }
     
-    private func getAVAssetImageGenerator(_ avAsset: AVAsset) -> AVAssetImageGenerator {
-        let generator = AVAssetImageGenerator(asset: avAsset)
+    private func getAVAssetImageGenerator() -> AVAssetImageGenerator {
+        let generator = AVAssetImageGenerator(asset: self.avAsset)
         generator.requestedTimeToleranceBefore = .zero
         generator.requestedTimeToleranceAfter = .zero
         generator.maximumSize = self.thumbnailImageSize!
@@ -51,34 +84,31 @@ public class DDThumbnailMaker {
         return generator
     }
     
-    private func getRequestedAssetTimes(_ avAsset: AVAsset) async -> [NSValue] {
+    private func getRequestedAssetTimes() async -> [NSValue] {
         // Get information about avAsset
-        let videoTracks = try! await avAsset.loadTracks(withMediaType: .video)
-        let frameRate = videoTracks.count > 0 ? try! await Int(videoTracks[0].load(.nominalFrameRate)) : 0
+        let durationValue = Float(self.duration.value)
+        let durationTimescale = Float(self.duration.timescale)
+        let durationFlags = self.duration.flags
+        let durationEpoch = self.duration.epoch
 
-        let value = try? await Float(avAsset.load(.duration).value)
-        let timeScale = try? await Float(avAsset.load(.duration).timescale)
-        let flags = try? await avAsset.load(.duration).flags
-        let epoch = try? await avAsset.load(.duration).epoch
-
-        let durationTimeMesc = Int(value!)*1000 / Int(timeScale!)
-        let numberOfFrames = Int(durationTimeMesc * frameRate)/1000
-        let timeValuePerFrame = Int(timeScale!) / frameRate
+        let durationTimeMesc = Int(durationValue) * 1000 / Int(durationTimescale)
+        let numberOfFrames = Int(durationTimeMesc * self.frameRate) / 1000
+        let timeValuePerFrame = Int(durationTimescale) / self.frameRate
         
         var times: [NSValue] = []
         for i in 1...numberOfFrames {
             let timeValue = UInt(timeValuePerFrame * i)
-            let timeStampMsec = UInt(timeValue * 1000) / UInt(timeScale!)
+            let timeStampMsec = UInt(timeValue * 1000) / UInt(durationTimescale)
 
             var shouldAppend = false
-            if self.intervalFrame! > 0 {
-                shouldAppend = (UInt(i) % self.intervalFrame! == 0)
+            if self.intervalFrame > 0 {
+                shouldAppend = (UInt(i) % self.intervalFrame == 0)
             } else {
-                shouldAppend = (timeStampMsec % self.intervalMsec! == 0)
+                shouldAppend = (timeStampMsec % self.intervalMsec == 0)
             }
             
             if i == 1 || shouldAppend {
-                let frameTime = CMTime(value: CMTimeValue(timeValue), timescale: CMTimeScale(timeScale!), flags: flags!, epoch: epoch!)
+                let frameTime = CMTime(value: CMTimeValue(timeValue), timescale: CMTimeScale(durationTimescale), flags: durationFlags, epoch: durationEpoch)
                 times.append(NSValue(time: frameTime))
             }
         }
